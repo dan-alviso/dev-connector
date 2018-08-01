3e699ca460e753fc43a475e1c54dc663970cb4d2
Daniel Alviso <dan.alviso@yahoo.com>
2018-07-31 17:40:15 -0700

    Added CreateProfile functionality

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,14 +2,7 @@ import React from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-const SelectListGroup = ({
-  name,
-  value,
-  error,
-  info,
-  onChange,
-  options
-}) => {
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
   const selectOptions = options.map(option => (
     <option key={option.label} value={option.value}>
       {option.label}
@@ -17,13 +10,14 @@ const SelectListGroup = ({
   ));
   return (
     <div className="form-group">
-      <select>
+      <select
         className={classnames("form-control form-control-lg", {
           "is-invalid": error
         })}
         name={name}
         value={value}
         onChange={onChange}
+      >
         {selectOptions}
       </select>
       {info && <small className="form-text text-muted">{info}</small>}
